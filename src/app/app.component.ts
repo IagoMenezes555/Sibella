c1b3e5503ce08fbdeec0cc628f4c300f27762f09
Iago <iagomenezes555@gmail.com>
2025-07-03 10:28:57 -0300

    feat: main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TextComponent } from './components/text/text.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, TextComponent],
+  imports: [RouterOutlet, HeaderComponent, TextComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
