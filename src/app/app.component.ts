27a66aec271bf3e13b75626efc50cbdb320111b5
Iago <iagomenezes555@gmail.com>
2025-07-02 23:07:01 -0300

    feat: text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { TextComponent } from './components/text/text.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TextComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
